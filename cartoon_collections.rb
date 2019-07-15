145c64cdd8b242a240aab743b6e71cac2f531f57
Natalie Taktachev <ntaktach@gmail.com>
2019-07-15 01:18:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(cheese)
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["Cheddar", "Gouda", "Camembert"]
   array.find { |a| a == "cheddar" || a == "gouda" || a == "camembert" }
